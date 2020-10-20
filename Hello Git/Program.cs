f1685811f0583d976a5fc3cdc431da1d44695d7c
Julio Briones <JBriones@wr.org>
2020-10-20 15:47:01 -0600

    Add project files.

---
+++ Hello Git/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello, my friend");
+            Console.Write("use github by project");
+        }
+    }
+}
