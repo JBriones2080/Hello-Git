9344948b9a3fbc37187ef37dc6c298c966740204
Julio Briones <JBriones@wr.org>
2020-10-21 14:01:17 -0600

    commit 03

--- Hello Git/Program.cs
+++ Hello Git/Program.cs
@@ -12,10 +12,69 @@ namespace Hello_Git
         {
             Console.WriteLine("Hello, my friend !");
             Console.WriteLine("use github by project");
-            Console.WriteLine("Press Enter key to continue...");
-            Console.ReadLine();
 
+            // Declare variables and then initialize to zero.
+            int num1 = 0; int num2 = 0;
 
+            // Display title as the C# console calculator app.
+            Console.WriteLine("Console Calculator in C#\r");
+            Console.WriteLine("------------------------\n");
+
+            // Ask the user to type the first number.
+            Console.WriteLine("Type a number, and then press Enter");
+
+           
+            num1 = Convert.ToInt32(ValidateNumbers(Console.ReadLine().ToString()));
+
+            // Ask the user to type the second number.
+            Console.WriteLine("Type another number, and then press Enter");
+            num2 = Convert.ToInt32(ValidateNumbers(Console.ReadLine().ToString()));
+
+            // Ask the user to choose an option.
+            Console.WriteLine("Choose an option from the following list:");
+            Console.WriteLine("\ta - Add");
+            Console.WriteLine("\ts - Subtract");
+            Console.WriteLine("\tm - Multiply");
+            Console.WriteLine("\td - Divide");
+            Console.Write("Your option? ");
+
+            // Use a switch statement to do the math.
+            switch (Console.ReadLine())
+            {
+                case "a":
+                    Console.WriteLine($"Your result: {num1} + {num2} = " + (num1 + num2));
+                    break;
+                case "s":
+                    Console.WriteLine($"Your result: {num1} - {num2} = " + (num1 - num2));
+                    break;
+                case "m":
+                    Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
+                    break;
+                case "d":
+                    Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
+                    break;
+            }
+            // Wait for the user to respond before closing.
+            Console.Write("Press any key to close the Calculator console app...");
+            Console.ReadKey(); 
+
+        }
+
+        private static int  ValidateNumbers(string readLine) {
+
+            int i = 0; 
+            bool result = int.TryParse(readLine, out i);  
+
+            if (result == true)
+            {
+                Console.WriteLine("number1 now = {0}", i);
+            }
+            else { 
+            
+                Console.WriteLine("numString is not a valid long");
+            }
+
+            return i;
         }
     }
 }
